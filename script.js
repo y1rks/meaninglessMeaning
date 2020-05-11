97475fd6d33125dbd8ba0930f1495d9fcc79bbb5
y1rks <yu.martian@gmail.com>
2020-05-12 01:48:44 +0900

    Add color changers

--- script.js
+++ script.js
@@ -18,3 +18,33 @@ function onButtonClick() {
       "I mean " + addingMessage;
   }
 }
+
+let redSwitchFlag = true;
+function onRedSwitchChange() {
+  if (redSwitchFlag) {
+    document.getElementById("square").style.backgroundColor = "#00dbeb";
+  } else {
+    document.getElementById("square").style.backgroundColor = "#eb1000";
+  }
+  redSwitchFlag = !redSwitchFlag;
+}
+
+let yellowSwitchFlag = true;
+function onYellowSwitchChange() {
+  if (yellowSwitchFlag) {
+    document.getElementById("triangle").style.borderBottomColor = "#0212fe";
+  } else {
+    document.getElementById("triangle").style.borderBottomColor = "#feee02";
+  }
+  yellowSwitchFlag = !yellowSwitchFlag;
+}
+
+let blueSwitchFlag = true;
+function onBlueSwitchChange() {
+  if (blueSwitchFlag) {
+    document.getElementById("circle").style.backgroundColor = "#cfba1a";
+  } else {
+    document.getElementById("circle").style.backgroundColor = "#1a2fcf";
+  }
+  blueSwitchFlag = !blueSwitchFlag;
+}
